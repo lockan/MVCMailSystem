2fedbabd9171349917d81b6b09e768788da291a7
lockan <lockan@gmail.com>
2015-03-17 20:10:52 -0700

    Selenium - Login Test sample code

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,7 +11,8 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         const string _appName =     @"MVCMailSystem";
-        const string _appPath =     @"E:\BCIT\Courses\Term4\COMP4941\Assignments\SeleniumDemo\MVCMailSystem\MVCMailSystem";
+        //const string _appPath =     @"E:\BCIT\Courses\Term4\COMP4941\Assignments\SeleniumDemo\MVCMailSystem\MVCMailSystem"; //desktop
+        const string _appPath = @"D:\BCIT\Courses\Term4\COMP4941\Assignments\MVCMailSystem\MVCMailSystem"; //laptop
         const string _iisPath =     @"C:\Program Files (x86)\IIS Express\iisexpress.exe";
         const int iisPort = 4444;  //Run the app, check the port, change it here. 
         string siteurl = "http://localhost:" + iisPort + "/";
@@ -19,8 +20,7 @@ namespace UnitTestProject1
         private Process iisProc;
         IWebDriver driver = new InternetExplorerDriver();
 
-        [TestInitialize]
-        public void TestInitialize()
+        private void initIIS()
         {
             // Start IIS on our specified port. 
             iisProc = new Process();
@@ -30,29 +30,66 @@ namespace UnitTestProject1
             System.Diagnostics.Debug.WriteLine("Running IIS: path={0} ; port={1}", _appPath, iisPort);
         }
 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            initIIS();
+        }
+
         [TestCleanup]
         public void TestCleanup()
-        {
+        {            
             // Ensure IISExpress is stopped
             if (iisProc.HasExited == false)
             {
                 iisProc.Kill();
             }
         }
-         
+
         [TestMethod]
-        public void LoginTest()
+        public void Login_Success()
         {
+            string expected = "http://localhost:4444/?username=Manager1";
+
             if (!iisProc.HasExited)
             {
+                //Test procedure
                 driver.Url = siteurl;
                 driver.Navigate().GoToUrl(siteurl + "Login/");
-                driver.FindElement(By.Id("username")).SendKeys("SeleniumTest");
+                driver.FindElement(By.Id("username")).SendKeys("Manager1");
+                driver.FindElement(By.Id("submit")).Click();
+
+                //Test condition: 
+                Assert.AreEqual(expected, driver.Url);
             }
             else
             {
                 System.Diagnostics.Debug.WriteLine("ERROR: Failed to launch IIS");
             }
         }
+         
+        [TestMethod]
+        public void Login_Failure()
+        {
+            string expected = @"http://localhost:4444/Login";
+            
+            if (!iisProc.HasExited)
+            {
+                //Test procedure
+                driver.Url = siteurl;
+                driver.Navigate().GoToUrl(siteurl + "Login/");
+                driver.FindElement(By.Id("username")).SendKeys("BadUserName");
+                driver.FindElement(By.Id("submit")).Click();   //By.id seems to work best - guarantees we get the element we're looking for. 
+                
+                //Test condition: 
+                Assert.AreEqual(expected, driver.Url);  //Asserts are a fairly standard way to check test success/failure in VS test project. 
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("ERROR: Failed to launch IIS");
+            }
+        }
+
     }
 }
