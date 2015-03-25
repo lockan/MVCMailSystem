01944daa2a85e8fb08e7bf255a378553ead45282
lockan <lockan@gmail.com>
2015-03-25 11:51:51 -0700

    Wonho's assets added to project as TreeView View/Controller

---
+++ MVCMailSystem/Controllers/TreeViewController.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Iteration1.DBContexts;
+using Iteration1.Models;
+
+namespace Iteration1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string employees)
+        {
+            ViewBag.SelectedEmployees = employees;
+
+            return View(db.Employees.ToList());
+        }
+    }
+}
