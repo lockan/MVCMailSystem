0ac4b0fd349977939ac933778465a2ff0d283136
lockan <lockan@gmail.com>
2015-03-25 12:44:18 -0700

    Wonho fixing errors in with jquery/jstree

--- MVCMailSystem/App_Start/BundleConfig.cs
+++ MVCMailSystem/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace MVCMailSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.11.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +26,10 @@ namespace MVCMailSystem
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jstree").Include(
+                        "~/Scripts/jquery-1.11.2.js",
+                        "~/Scripts/jstree.js"));   
         }
     }
 }
