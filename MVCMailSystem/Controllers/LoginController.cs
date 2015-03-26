682ba40af54eda66b50fcb6cd566f568d77532d6
lockan <lockan@gmail.com>
2015-03-26 15:19:07 -0700

    Fix for Mr. ThePlague's stafftype

--- MVCMailSystem/Controllers/LoginController.cs
+++ MVCMailSystem/Controllers/LoginController.cs
@@ -20,10 +20,17 @@ namespace MVCMailSystem.Controllers
             if (TempData["errorMessage"] != null) @ViewBag.errorMessage = TempData["errorMessage"].ToString();
             //SeleniumDriver selenium = new SeleniumDriver();
             //selenium.LoginTest();
+            List<SelectListItem> arbitrarylist = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Arbitrary 1", Value="Arbitrary1" },
+                new SelectListItem { Text = "Arbitrary 2", Value="Arbitrary2" },
+                new SelectListItem { Text = "Arbitrary 3", Value="Arbitrary3" }
+            };
+            ViewBag.alist = arbitrarylist;
             return View();
         }
 
-        public ActionResult Login(string username)
+        public ActionResult Login(string username, string ArbitraryList)
         {            
             
             Employee emp = db.empDB.SingleOrDefault(user => user.username == username);
